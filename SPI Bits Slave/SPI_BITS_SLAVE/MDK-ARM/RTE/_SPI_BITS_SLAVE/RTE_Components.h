b6a7685352aa007f3a457f3964b4b3e2417ee7ca
Adi Okerić <adi_okeric@hotmail.com>
2024-02-04 23:55:38 +0100

    SPI Bits Slave

---
+++ SPI Bits Slave/SPI_BITS_SLAVE/MDK-ARM/RTE/_SPI_BITS_SLAVE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SPI_BITS_SLAVE' 
+ * Target:  'SPI_BITS_SLAVE' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
